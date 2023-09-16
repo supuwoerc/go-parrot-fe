2662514e7c4d56653465ec8261c4f250c271b37d
zhangqm <qimeng_zhang@intsig.net>
2023-09-16 22:46:54 +0800

    feat: 添加全局样式

--- src/components/reset.tsx
+++ src/components/reset.tsx
@@ -1,19 +1,16 @@
-import { colors } from '@/style/color'
-import { resetCss } from '@/style/reset'
 import { Global, css } from '@emotion/react'
-
 // 全局的样式
-export const GlobalStyles: React.FC = () => {
+export const ResetStyles: React.FC = () => {
     return (
         <Global
             styles={css`
                 html,
                 body,
                 * {
+                    padding: 0;
+                    margin: 0;
                     box-sizing: border-box;
                 }
-                ${colors}
-                ${resetCss}
                 #root {
                     height: 100%;
                     margin: 0;
---
+++ src/style/color.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react'
+
+// 全局的颜色定义
+export const colors = css`
+    :root:root {
+        --theme: #1677ff;
+        --warn: #faad14;
+        --error: #ff4d4f;
+    }
+`
